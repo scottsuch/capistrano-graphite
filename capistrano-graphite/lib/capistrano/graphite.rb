0d101c95d110e72c625881d3b3ad33ed9dfdd172
scottsuch <sgorsuch@gmail.com>
2014-04-16 13:57:12 -0400

    Initial commit

---
+++ capistrano-graphite/lib/capistrano/graphite.rb
@@ -0,0 +1,7 @@
+require "capistrano/graphite/version"
+
+module Capistrano
+  module Graphite
+    # Your code goes here...
+  end
+end
---
+++ capistrano-graphite/lib/capistrano/graphite/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Graphite
+    VERSION = "0.0.1"
+  end
+end
